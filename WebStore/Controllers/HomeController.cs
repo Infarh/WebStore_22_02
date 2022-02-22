c6bf2d8964a310f4423df00de2bf3dc9f985c0ea
Infarh <shmachilin@yandex.ru>
2022-02-22 20:59:44 +0300

    Работа с моделями в представлениях

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,26 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
-namespace WebStore.Controllers
+namespace WebStore.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private static readonly List<Employee> __Employees = new()
     {
-        private readonly IConfiguration _Configuration;
+        new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 23 },
+        new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 27 },
+        new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+    };
 
-        public HomeController(IConfiguration Configuration) { _Configuration = Configuration; }
+    private readonly IConfiguration _Configuration;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public HomeController(IConfiguration Configuration) { _Configuration = Configuration; }
 
-        public IActionResult ContentString(string Id = "-id-")
-        {
-            return Content($"content: {Id}");
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult ConfigStr()
-        {
-            return Content($"config: {_Configuration["ServerGreetings"]}");
-        }
+    public IActionResult ContentString(string Id = "-id-")
+    {
+        return Content($"content: {Id}");
+    }
+
+    public IActionResult ConfigStr()
+    {
+        return Content($"config: {_Configuration["ServerGreetings"]}");
+    }
+
+    public IActionResult Employees()
+    {
+        return View(__Employees);
     }
 }
--- WebStore/Models/Employee.cs
+++ WebStore/Models/Employee.cs
@@ -1,4 +1,8 @@
-﻿namespace WebStore.Models;
+﻿using System.Text;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.VisualBasic;
+
+namespace WebStore.Models;
 
 public class Employee
 {
@@ -8,5 +12,31 @@ public class Employee
     public string FirstName { get; set; }
     public string Patronymic { get; set; }
 
-    public int Age { get; set; }
+    public string ShortName
+    {
+        get
+        {
+            var result = new StringBuilder(LastName);
+
+            if (FirstName is { Length: > 0 } first_name)
+                result.Append(" ").Append(first_name[0]).Append(".");
+
+            if (Patronymic is { Length: > 0 } patronymic)
+                result.Append(" ").Append(patronymic[0]).Append(".");
+
+            return result.ToString();
+        }
+    }
+
+    private int _Age;
+    public int Age
+    {
+        get => _Age;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentOutOfRangeException(nameof(value), value, "Значение возраста не может быть меньше 0");
+            _Age = value;
+        }
+    }
 }
