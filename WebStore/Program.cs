264c76202645898e95acce72d38edf3e5b6eaf7e
Infarh <shmachilin@yandex.ru>
2022-02-22 20:31:39 +0300

    Определение маршрутов к контроллерам

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -5,6 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.UseRouting();
+
 app.MapGet("/", () => app.Configuration["ServerGreetings"]);
+
+app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
