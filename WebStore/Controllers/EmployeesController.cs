a1faa6bdebcbaca50fb769ba55b8cb2de40112eb
Infarh <shmachilin@yandex.ru>
2022-03-01 20:44:30 +0300

    Заготовка основных операций над сотрудниками

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -33,4 +33,19 @@ public class EmployeesController : Controller
 
         return View(employee);
     }
+
+    //public IActionResult Add()
+    //{
+    //    return View();
+    //}
+
+    public IActionResult Edit(int id)
+    {
+        return View();
+    }
+
+    //public IActionResult Delete(int id)
+    //{
+    //    return View();
+    //}
 }
