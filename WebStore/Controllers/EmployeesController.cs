a77005e7d80bfa34ff83e1a408afed7b7a53c488
Infarh <shmachilin@yandex.ru>
2022-03-01 21:15:50 +0300

    Добавление нового сотрудника

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
 using WebStore.Models;
 using WebStore.Services.Interfaces;
 using WebStore.ViewModels;
@@ -35,13 +34,16 @@ public class EmployeesController : Controller
         return View(employee);
     }
 
-    //public IActionResult Add()
-    //{
-    //    return View();
-    //}
-
-    public IActionResult Edit(int id)
+    public IActionResult Create()
     {
+        return View("Edit", new EmployeesViewModel());
+    }
+
+    public IActionResult Edit(int? Id)
+    {
+        if (Id is not { } id)
+            return View(new EmployeesViewModel());
+
         var employee = _EmployeesData.GetById(id);
         if (employee is null)
             return NotFound();
@@ -71,13 +73,45 @@ public class EmployeesController : Controller
             Age = Model.Age,
         };
 
+        if (Model.Id == 0)
+        {
+            var id = _EmployeesData.Add(employee);
+            return RedirectToAction(nameof(Details), new { id });
+        }
+
         _EmployeesData.Edit(employee);
 
         return RedirectToAction(nameof(Index));
     }
 
-    //public IActionResult Delete(int id)
-    //{
-    //    return View();
-    //}
+    public IActionResult Delete(int id)
+    {
+        if (id <= 0)
+            return BadRequest();
+
+        var employee = _EmployeesData.GetById(id);
+        if (employee is null)
+            return NotFound();
+
+        var model = new EmployeesViewModel
+        {
+            Id = employee.Id,
+            LastName = employee.LastName,
+            FirstName = employee.FirstName,
+            Patronymic = employee.Patronymic,
+            ShortName = employee.ShortName,
+            Age = employee.Age,
+        };
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult DeleteConfirmed(int Id)
+    {
+        if (!_EmployeesData.Delete(Id))
+            return NotFound();
+
+        return RedirectToAction(nameof(Index));
+    }
 }
