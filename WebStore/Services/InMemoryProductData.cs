eff569cadfd528652797b42f3beb6815c1a552cb
Infarh <shmachilin@yandex.ru>
2022-03-04 20:53:17 +0300

    Товары

--- WebStore/Services/InMemoryProductData.cs
+++ WebStore/Services/InMemoryProductData.cs
@@ -1,4 +1,5 @@
 ﻿using WebStore.Data;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Services.Interfaces;
 
@@ -9,4 +10,20 @@ public class InMemoryProductData : IProductData
     public IEnumerable<Section> GetSections() => TestData.Sections;
 
     public IEnumerable<Brand> GetBrands() => TestData.Brands;
+
+    public IEnumerable<Product> GetProducts(ProductFilter? Filer = null)
+    {
+        IEnumerable<Product> query = TestData.Products;
+
+        //if (Filer != null && Filer.SectionId != null)
+        //    query = query.Where(p => p.SectionId == Filer.SectionId);
+
+        if (Filer?.SectionId is { } section_id)
+            query = query.Where(p => p.SectionId == section_id);
+
+        if (Filer?.BrandId is { } brand_id)
+            query = query.Where(p => p.BrandId == brand_id);
+
+        return query;
+    }
 }
