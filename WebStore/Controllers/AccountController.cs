2112269aaa886e5470d851a53f1017905cc144f5
Infarh <shmachilin@yandex.ru>
2022-03-22 20:51:18 +0300

    Логика входа в систему

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -57,9 +57,34 @@ public class AccountController : Controller
         if (!ModelState.IsValid)
             return View(Model);
 
+        var login_result = await _SignInManager.PasswordSignInAsync(
+            Model.UserName,
+            Model.Password,
+            Model.RememberMe,
+            true);
 
+        if (login_result.Succeeded)
+        {
+            //return Redirect(Model.ReturnUrl); // Не безопасно!!!
 
-        return RedirectToAction("Index", "Home");
+            //if(Url.IsLocalUrl(Model.ReturnUrl))
+            //    return Redirect(Model.ReturnUrl);
+            //return RedirectToAction("Index", "Home");
+
+            return LocalRedirect(Model.ReturnUrl ?? "/");
+        }
+        //else if (login_result.RequiresTwoFactor)
+        //{
+        //    // выполнить двухфакторную авторизацию
+        //}
+        //else if (login_result.IsLockedOut)
+        //{
+        //    // отправить пользователю информаию о том, что он заблокирован
+        //}
+
+        ModelState.AddModelError("", "Неверное имя пользователя, или пароль");
+
+        return View(Model);
     }
 
     public async Task<IActionResult> Logout()
