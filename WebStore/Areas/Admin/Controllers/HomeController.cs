d93cd0dc27cd38b4f48f27716134404b2e1fccb4
Infarh <shmachilin@yandex.ru>
2022-03-29 19:33:34 +0300

    Заготовка инфраструктуры MVC в области администратора

--- WebStore/Areas/Admin/Controllers/HomeController.cs
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore.Areas.Admin.Controllers;
 
 //[Area("Admin")]
+//[Authorize]
 public class HomeController : Controller
 {
-    public IActionResult Index() => Content("Admin controller");
+    public IActionResult Index() => View();
 }
