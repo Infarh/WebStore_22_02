4047b4183b88a16330a29973b600bad9a8acfd85
Infarh <shmachilin@yandex.ru>
2022-04-12 19:44:28 +0300

    Проверка работоспособности контроллеров

--- Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
+++ Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 using WebStore.Interfaces;
 
 namespace WebStore.WebAPI.Controllers.Identity;
@@ -7,5 +11,15 @@ namespace WebStore.WebAPI.Controllers.Identity;
 [Route(WebAPIAddresses.V1.Identity.Users)]
 public class UsersApiController : ControllerBase
 {
+    private readonly ILogger<UsersApiController> _Logger;
+    private readonly UserStore<User, Role, WebStoreDB> _UserStore;
 
+    public UsersApiController(WebStoreDB db, ILogger<UsersApiController> Logger)
+    {
+        _Logger = Logger;
+        _UserStore = new(db);
+    }
+
+    [HttpGet("all")]
+    public async Task<IEnumerable<User>> GetAll() => await _UserStore.Users.ToArrayAsync();
 }
