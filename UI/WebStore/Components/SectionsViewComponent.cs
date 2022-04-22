a4aac087a11169a9b16a1f743a054b52257bb42a
Infarh <shmachilin@yandex.ru>
2022-04-22 20:02:27 +0300

    Подсветка активной секции

--- UI/WebStore/Components/SectionsViewComponent.cs
+++ UI/WebStore/Components/SectionsViewComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+
 using WebStore.Domain.ViewModels;
 using WebStore.Interfaces.Services;
 
@@ -10,19 +11,37 @@ public class SectionsViewComponent : ViewComponent
 
     public SectionsViewComponent(IProductData ProductData) => _ProductData = ProductData;
 
-    public IViewComponentResult Invoke()
+    public IViewComponentResult Invoke(string SectionId)
     {
+        //var section_id_str = HttpContext.Request.Query["SectionId"];
+
+        var section_id = int.TryParse(SectionId, out var id) ? id : (int?)null;
+
+        var sections = GetSections(section_id, out var parent_section);
+
+        return View(new SelectableSectionsViewModel
+        {
+            Sections = sections,
+            SectionId = section_id,
+            ParentSectionId = parent_section,
+        });
+    }
+
+    private IEnumerable<SectionViewModel> GetSections(int? SectionId, out int? ParentSectionId)
+    {
+        ParentSectionId = null;
+
         var sections = _ProductData.GetSections();
 
         var parent_sections = sections.Where(s => s.ParentId is null);
 
         var parent_sections_views = parent_sections
            .Select(s => new SectionViewModel
-            {
-                Id = s.Id,
-                Name = s.Name,
-                Order = s.Order,
-            })
+           {
+               Id = s.Id,
+               Name = s.Name,
+               Order = s.Order,
+           })
            .ToList();
 
         foreach (var parent_section in parent_sections_views)
@@ -30,18 +49,23 @@ public class SectionsViewComponent : ViewComponent
             var childs = sections.Where(s => s.ParentId == parent_section.Id);
 
             foreach (var child_section in childs)
+            {
+                if (child_section.Id == SectionId)
+                    ParentSectionId = child_section.ParentId;
+
                 parent_section.ChildSections.Add(new()
                 {
                     Id = child_section.Id,
                     Name = child_section.Name,
                     Order = child_section.Order,
                 });
+            }
 
             parent_section.ChildSections.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
         }
 
         parent_sections_views.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
 
-        return View(parent_sections_views);
+        return parent_sections_views;
     }
 }
