85e5e08e6448406a5f224e3d3a46100587a019cb
Infarh <shmachilin@yandex.ru>
2022-04-26 21:19:49 +0300

    Визуальная часть разбиения каталога товаров на страницы

--- Common/WebStore.Domain/ViewModels/CatalogViewModel.cs
+++ Common/WebStore.Domain/ViewModels/CatalogViewModel.cs
@@ -2,9 +2,11 @@
 
 public class CatalogViewModel
 {
-    public int? BrandId { get; set; }
+    public int? BrandId { get; init; }
 
-    public int? SectionId { get; set; }
+    public int? SectionId { get; init; }
 
-    public IEnumerable<ProductViewModel> Products { get; set; } = Enumerable.Empty<ProductViewModel>();
+    public IEnumerable<ProductViewModel> Products { get; init; } = Enumerable.Empty<ProductViewModel>();
+
+    public PageViewModel PageModel { get; init; } = null!;
 }
