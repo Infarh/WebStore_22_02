f68c3564def2bf31af342ed24e54f758e78dae40
Infarh <shmachilin@yandex.ru>
2022-04-26 19:55:16 +0300

    Исправление ошибки в скрипте

--- UI/WebStore/wwwroot/js/Cart/CartLogic.js
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -13,7 +13,7 @@
     addToCart: function(e) {
         e.preventDefault();
 
-        const button = $(this);
+        var button = $(this);
         const id = button.data("id");
 
         $.get(Cart._properties.addToCartLink + "/" + id)
@@ -27,7 +27,7 @@
     },
 
     showToolTip: function(button) {
-        button.tooltip({ title: "Добавлено в корзину" }.tooltip("show"));
+        button.tooltip({ title: "Добавлено в корзину" }).tooltip("show");
         setTimeout(function() {
                 button.tooltip("destroy");
             },
